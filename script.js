bc43441d7b0117aa0e4b036fb73057b0a9c55500
Chamizal1 <nserratoa2200@alumno.ipn.mx>
2024-09-24 17:55:23 -0600

    Creacion de directorio de archivo

---
+++ script.js
@@ -0,0 +1,29 @@
+//Clase 24/09/24
+//Serrato Ascencio Nestor Aldair
+//variables
+/*
+  las variables guardan informacion que puedes reutilizar
+ */
+
+  //declaracion de variables 
+
+  let nombre = 'pedro'; //varibles que puden cambiar su valor
+  const PI = 3.14; //variables que no puden cambiar su valor
+
+
+  //operadores
+  let a = 5;
+  let b = 2;
+  let suma = a+ b;
+  let srsta = a-b;
+  let multi = a*b;
+  let division = a/b
+
+console.log(suma, srsta, multi, division);
+
+//operadores de comparacion
+
+a >b;
+a<b;
+a == b;
+a ===b; //compara tipos y valor
