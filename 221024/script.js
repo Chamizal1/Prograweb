9d179090ea6f18c5a6364beff5c1df6e04f834f1
Chamizal1 <160682485+Chamizal1@users.noreply.github.com>
2024-10-22 17:43:29 -0600

    221024

---
+++ 221024/script.js
@@ -0,0 +1,22 @@
+function s(a,b){
+ return a+b;
+}
+console.log(s(3,5));
+
+
+//flecha
+
+const suma = (a,b)=>{
+    return a+b;
+}
+console.log(suma(3,5));
+
+//simplifica
+
+const suma1 = (a,b) => a+b;
+console.log(suma1(5,5));
+
+//solo un parametro
+
+const cuadrado = x => x*x;
+console.log(cuadrado(5));
