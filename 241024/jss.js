5a7c025f78f644bbe10d9f136f6293321d044b43
Chamizal1 <160682485+Chamizal1@users.noreply.github.com>
2024-10-24 16:38:21 -0600

    241024

---
+++ 241024/jss.js
@@ -0,0 +1,36 @@
+/*
+let array = ["dato1", 2, Boolean];
+console.log("tamaño del array:" + array.length);
+array.push("te transformas");
+console.log("insertar nuevo valor");
+console.log(array.length);
+*/
+/*
+const array = []
+for(let i = 0; i<10; i++){
+    array.push(5*(i+1));
+    console.log(`5*${i+1} = ${array[i]}`);
+
+    if(array.length === 6){
+        console.log("Arreglo en la posicion 6 ="+ array[i]);
+        break;
+    }   
+}*/
+/*  
+let array = ["dato1", "DAto 2", "Dato 3"];
+array.push("Dato 4");
+console.log(array[3]);
+
+//eliminar
+
+array.splice(1,1);
+console.log(array);*/
+
+const input = document.getElementById("input");
+const button = document.getElementById("btn");
+const arrglo = []
+
+function agregar(){
+    const valor = input.value;
+}
+
